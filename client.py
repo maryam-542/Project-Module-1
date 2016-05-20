8ccc5c004a0959c55142616b4169b00f2455b3d0
maryam-542 <bsef14m542@pucit.edu.pk>
2016-05-20 09:46:26 +0500

    Update client.py

--- client.py
+++ client.py
@@ -5,21 +5,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket Created'
          
 host = socket.gethostname()
-port = 5182                # Reserve a port for your service.
+port = 5188                # Reserve a port for your service.
 
 s.connect((host, port))
-data="aslam o alaikum wr wb\n"
 
-
- 
-#Bind socket to local host and port
-try:
-    s.sendall(data)
-except socket.error as msg:
-    print 'Bind failed. Error in Message sending!'
-    sys.exit()
-
-print 'Message Sent Alhamdulillah :) '
 print s.recv(1024)
 
 s.close                     # Close the socket when done
